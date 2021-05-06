1c21412e0c922d7957e3ded429e2dd933174b100
nikhilbghodke <nikhil.ghodke17@vit.edu>
2021-05-06 13:01:11 +0530

    feat: app builder config picked up from .env file

--- src/App.js
+++ src/App.js
@@ -6,19 +6,29 @@ import { AppContextProvider } from "./store/AppContext.js";
 import { HMSRoomProvider, HMSThemeProvider } from "@100mslive/sdk-components";
 
 function App() {
+  const { 0: width, 1: height } = process.env.REACT_APP_TILE_SHAPE.split(
+    "-"
+  ).map((el) => parseInt(el));
   return (
     <div className="w-full h-screen dark:bg-black">
       <HMSThemeProvider
-        theme={{}}
-        appBuilder={{
-          theme: "light",
-          // enableChat: false,
-          // enableScreenShare: false,
-          logo: "https://blog.hubspot.com/hubfs/image8-2.jpg",
-          videoTileAspectRatio: {
-            width: 1,
-            height: 1,
+        config={{
+          theme: {
+            extend: {
+              fontFamily: {
+                sans: [process.env.REACT_APP_FONT, "Inter", "sans-serif"],
+                body: [process.env.REACT_APP_FONT, "Inter", "sans-serif"],
+              },
+            },
           },
+        }}
+        appBuilder={{
+          theme: process.env.REACT_APP_THEME,
+          enableChat: process.env.REACT_APP_SHOW_CHAT,
+          enableScreenShare: process.env.REACT_APP_SHOW_SCREENSHARE,
+          logo: process.env.REACT_APP_LOGO,
+          videoTileAspectRatio: { width, height },
+          showAvatar: process.env.REACT_APP_VIDEO_AVATAR,
         }}
       >
         <HMSRoomProvider>
