3f0d17a8634bf571ad5b56f984b8afd5fe7a27d4
Tushar <tushar@100ms.live>
2021-05-21 20:18:31 +0530

    feat(store): simplify use of components

--- src/views/screenShareView.jsx
+++ src/views/screenShareView.jsx
@@ -3,124 +3,86 @@ import {
   VideoList,
   VideoTile,
   selectPeers,
+  selectPeerScreenSharing,
 } from "@100mslive/sdk-components";
-import React from "react";
-import { getStreamsInfo } from "../utlis/index";
+import React, {useMemo} from "react";
 import { ChatView } from "./chatView";
 
+export const ScreenShareView = ({ isChatOpen, toggleChat }) => {
+  const peers = useHMSStore(selectPeers);
+  const peerScreenSharing = useHMSStore(selectPeerScreenSharing);
+
+  const ScreenShareComponent = <div className="w-8/10 h-full">
+    {peerScreenSharing && (
+        <VideoTile peer={peerScreenSharing} showScreen={true} objectFit="contain"/>
+    )}
+  </div>
+
+  return (
+      <React.Fragment>
+        <div className="w-full h-full flex">
+          <ScreenShareComponent/>
+          <div className="flex flex-wrap overflow-hidden p-2 w-2/10 h-full ">
+            <SidePane
+                isChatOpen={isChatOpen}
+                toggleChat={toggleChat}
+                peerScreenSharing={peerScreenSharing}
+                allPeers={peers}
+            />
+          </div>
+        </div>
+      </React.Fragment>
+  );
+};
+
 const SidePane = ({
-  streamsWithInfo,
   isChatOpen,
-  cameraStream,
   toggleChat,
+  peerScreenSharing,
+  allPeers
 }) => {
+  const remainingPeers = useMemo(() => allPeers.filter(peer => peer.id !== peerScreenSharing.id),
+      [allPeers, peerScreenSharing]);
+
+  const PeerScreenSharingView = <div className="w-full relative overflow-hidden"
+      style={{
+        paddingTop: `${peerScreenSharing ? "100%" : "0"}`,
+      }}>
+    {peerScreenSharing && (
+        <div className="absolute left-0 top-0 w-full h-full p-3">
+          <VideoTile peer={peerScreenSharing}/>
+        </div>
+    )}
+  </div>
+
+  // The main screen is already being shown
+  const shouldShowScreen = (peer) => peer.id !== peerScreenSharing.id;
+
+  const RemainingPeersView = <div className={`w-full relative ${isChatOpen ? "h-1/3" : "flex-grow"}`}>
+    {remainingPeers && (
+        <VideoList
+            peers={isChatOpen ? [peerScreenSharing, ...remainingPeers] : remainingPeers}
+            showScreen={shouldShowScreen}
+            classes={{videoTileContainer: "rounded-lg p-2"}}
+            maxColCount={2}
+            overflow="scroll-x"
+        />
+    )}
+  </div>
+
+  const CustomChatView = isChatOpen && <div className="h-2/3 w-full absolute z-40 bottom-0 right-0">
+        <ChatView toggleChat={toggleChat} />
+      </div>
+
   return (
     <>
       <div className={`w-full h-full relative`}>
         <div className={`w-full flex flex-col h-full`}>
-          <div
-            className="w-full relative overflow-hidden"
-            style={{
-              paddingTop: `${cameraStream ? "100%" : "0"}`,
-            }}
-          >
-            {cameraStream && (
-              <div className="absolute left-0 top-0 w-full h-full p-3">
-                <VideoTile
-                  audioTrack={cameraStream.audioTrack}
-                  hmsVideoTrack={cameraStream.hmsVideoTrack}
-                  videoTrack={cameraStream.videoTrack}
-                  peer={cameraStream.peer}
-                  showAudioMuteStatus
-                  allowRemoteMute
-                  isLocal={cameraStream.isLocal}
-                  isAudioMuted={
-                    !(
-                      cameraStream.audioTrack && cameraStream.audioTrack.enabled
-                    )
-                  }
-                  isVideoMuted={
-                    !(
-                      cameraStream.videoTrack && cameraStream.videoTrack.enabled
-                    )
-                  }
-                  audioLevel={cameraStream.audioLevel}
-                />
-              </div>
-            )}
-          </div>
-          <div
-            className={`w-full relative ${isChatOpen ? "h-1/3" : "flex-grow"}`}
-          >
-            {streamsWithInfo && streamsWithInfo.length > 0 && (
-              <VideoList
-                streams={
-                  isChatOpen
-                    ? [cameraStream, ...streamsWithInfo]
-                    : streamsWithInfo
-                }
-                classes={{
-                  videoTileContainer: "rounded-lg p-2",
-                }}
-                showAudioMuteStatus={true}
-                allowRemoteMute
-                maxColCount={2}
-                overflow="scroll-x"
-              />
-            )}
-          </div>
+          <PeerScreenSharingView/>
+          <RemainingPeersView/>
         </div>
-        {isChatOpen && (
-          <div className="h-2/3 w-full absolute z-40 bottom-0 right-0">
-            <ChatView toggleChat={toggleChat} />
-          </div>
-        )}
+        <CustomChatView/>
       </div>
     </>
-  );
-};
-
-export const ScreenShareView = ({ isChatOpen, toggleChat }) => {
-  const peers = useHMSStore(selectPeers);
-
-  const { streamsWithInfo, screenStream, cameraStream } = getStreamsInfo({
-    peers,
-  });
-
-  return (
-    <React.Fragment>
-      <div className="w-full h-full flex">
-        <div className="w-8/10 h-full">
-          {screenStream && screenStream.videoTrack && screenStream.hmsVideoTrack && (
-            <VideoTile
-              audioTrack={screenStream.audioTrack}
-              hmsVideoTrack={screenStream.hmsVideoTrack}
-              videoTrack={screenStream.videoTrack}
-              peer={screenStream.peer}
-              videoSource="screen"
-              showAudioMuteStatus={false}
-              allowRemoteMute={false}
-              isLocal={screenStream.isLocal}
-              objectFit="contain"
-              isAudioMuted={
-                !(screenStream.audioTrack && screenStream.audioTrack.enabled)
-              }
-              isVideoMuted={
-                !(screenStream.videoTrack && screenStream.videoTrack.enabled)
-              }
-            />
-          )}
-        </div>
-
-        <div className="flex flex-wrap overflow-hidden p-2 w-2/10 h-full ">
-          <SidePane
-            streamsWithInfo={streamsWithInfo}
-            isChatOpen={isChatOpen}
-            cameraStream={cameraStream}
-            toggleChat={toggleChat}
-          />
-        </div>
-      </div>
-    </React.Fragment>
   );
 };
