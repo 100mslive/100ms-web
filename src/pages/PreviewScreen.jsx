3fbb0e667c8bf5c45a11e0e6dfa9bf36a0d8c455
vivek9patel <vivek.p9737@gmail.com>
2021-06-11 22:26:08 +0530

    feat(roomUrl): adds new room-url support

--- src/pages/PreviewScreen.jsx
+++ src/pages/PreviewScreen.jsx
@@ -4,7 +4,7 @@ import { Preview } from "@100mslive/hms-video-react";
 import { AppContext } from "../store/AppContext";
 import getToken from "../services/tokenService";
 
-const PreviewScreen = () => {
+const PreviewScreen = ({ getUserToken }) => {
   const history = useHistory();
   const context = useContext(AppContext);
   const { loginInfo, setLoginInfo, setMaxTileCount, tokenEndpoint } = context;
@@ -12,23 +12,43 @@ const PreviewScreen = () => {
   const location = useLocation();
 
   const join = ({ audioMuted, videoMuted, name }) => {
-    return getToken(tokenEndpoint, loginInfo.env, name, userRole, urlRoomId)
-      .then(token => {
+    if (!userRole) {
+      getUserToken(name).then((token) => {
         setLoginInfo({
           token,
           audioMuted,
           videoMuted,
-          role: userRole,
           roomId: urlRoomId,
           username: name,
         });
-        // send to meeting room now
-        history.push(`/meeting/${urlRoomId}/${userRole}`);
+        if (userRole) history.push(`/meeting/${urlRoomId}/${userRole}`);
+        else history.push(`/meeting/${urlRoomId}`);
       })
-      .catch(error => {
-        console.log("Token API Error", error);
-      });
+        .catch(error => {
+          console.log("Token API Error", error);
+        });
+    }
+    else {
+      getToken(tokenEndpoint, loginInfo.env, name, userRole, urlRoomId)
+        .then(token => {
+          setLoginInfo({
+            token,
+            audioMuted,
+            videoMuted,
+            role: userRole,
+            roomId: urlRoomId,
+            username: name,
+          });
+          // send to meeting room now
+          if (userRole) history.push(`/meeting/${urlRoomId}/${userRole}`);
+          else history.push(`/meeting/${urlRoomId}`);
+        })
+        .catch(error => {
+          console.log("Token API Error", error);
+        });
+    }
   };
+
   const onChange = ({
     selectedVideoInput,
     selectedAudioInput,
@@ -78,9 +98,35 @@ const PreviewScreen = () => {
             onChange={onChange}
           />
         </div>
-      </div>
-    );
+      );
+    }
   }
+  else {
+    if (urlRoomId === "preview" || // '/<preview|meeting|leave>'
+      urlRoomId === "meeting" ||
+      urlRoomId === "leave"
+    ) {
+      history.push(`/`);
+    }
+    else if (!isPreview) {
+      history.push(`/preview/${urlRoomId}`);
+    }
+    else {
+      return (
+        <div className="h-full">
+          <div className="flex justify-center h-full items-center">
+            <Preview
+              joinOnClick={join}
+              goBackOnClick={goBack}
+              messageOnClose={goBack}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      );
+    }
+  }
+
   return null;
 };
 
--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 export default async function getToken(
   tokenEndpoint,
   env,
