6ca7cf2fd5fad76a7a67fbca321121245532a53a
raviteja83 <ravi.theja@100ms.live>
2021-06-22 12:34:19 +0530

    fix: typo in prop

--- src/pages/conference.jsx
+++ src/pages/conference.jsx
@@ -47,7 +47,7 @@ export const Conference = () => {
         <ConferenceMainView
           isChatOpen={isChatOpen}
           isParticipantListOpen={isParticipantListOpen}
-          togtoggleChat={toggleChat}
+          toggleChat={toggleChat}
         />
         <Notifications />
       </div>
