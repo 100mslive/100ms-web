f2f06d2938993c6c65a35eccb12cd0b9cd5a2356
Ashish1793 <ashish@100ms.live>
2021-07-13 19:04:25 +0530

    wip : vb2.0 wip

--- src/views/mainView.jsx
+++ src/views/mainView.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react";
-import background from './lukas.jpg';
 
 import {
   selectLocalPeer,
@@ -9,7 +8,6 @@ import {
 } from "@100mslive/hms-video-react";
 import { ScreenShareView } from "./screenShareView";
 import { MainGridView } from "./mainGridView";
-// import {HMSBackgroundProcessor} from "@100mslive/hms-virtual-background";
 
 export const ConferenceMainView = ({
   isChatOpen,
@@ -18,21 +16,6 @@ export const ConferenceMainView = ({
 }) => {
   const localPeer = useHMSStore(selectLocalPeer);
   const isSomeoneScreenSharing = useHMSStore(selectIsSomeoneScreenSharing);
-  const hmsActions = useHMSActions();
-
-  // async function startProcessor(){
-  //   const processor = new HMSBackgroundProcessor('blur', 30);
-  //   window.BGPROCESSOR = processor;
-  //   console.log("Processor", processor);
-  //   window.HMSACTION = hmsActions;
-  //   await hmsActions.addVideoProcessor(processor);
-  // }
-  //
-  // useEffect(() =>{
-  //   if(localPeer && localPeer.videoTrack){
-  //    // startProcessor();
-  //   }
-  // },[localPeer]);
 
   if (!localPeer) {
     // we don't know the role yet to decide how to render UI
