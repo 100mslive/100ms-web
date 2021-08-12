b5e57d3609ed446387d590c311e2d0bd57fae5b6
raviteja83 <ravi.theja@100ms.live>
2021-08-12 19:18:57 +0530

    fix: update selector

--- src/views/footerView.jsx
+++ src/views/footerView.jsx
@@ -23,7 +23,7 @@ import {
   isMobileDevice,
   selectIsAllowedToPublish,
   selectIsLocalVideoPluginPresent,
-  selectIsAllowedToEndRoom,
+  selectPermissions,
 } from "@100mslive/hms-video-react";
 import { useHistory, useParams } from "react-router-dom";
 import { HMSVirtualBackgroundPlugin } from "@100mslive/hms-virtual-background";
@@ -61,7 +61,7 @@ export const ConferenceFooter = ({ isChatOpen, toggleChat }) => {
   const params = useParams();
   const pluginRef = useRef(null);
   const isAllowedToPublish = useHMSStore(selectIsAllowedToPublish);
-  const isAllowedToEndRoom = useHMSStore(selectIsAllowedToEndRoom);
+  const { endRoom: isAllowedToEndRoom } = useHMSStore(selectPermissions);
 
   const initialModalProps = {
     show: false,
