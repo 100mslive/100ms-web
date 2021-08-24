a8c1a8135f54c2f48835b57f8729813894c96c07
raviteja83 <ravi.theja@100ms.live>
2021-08-24 15:09:16 +0530

    fix: update height of body and root

--- src/store/AppContext.js
+++ src/store/AppContext.js
@@ -51,19 +51,6 @@ const AppContextProvider = ({
   }, [hmsActions]);
 
   useEffect(() => {
-    const handleResize = () => {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-    handleResize();
-    // We listen to the resize event
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
     if (!state.loginInfo.token) return;
     hmsActions.join(convertLoginInfoToJoinConfig(state.loginInfo));
     // eslint-disable-next-line
