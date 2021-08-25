bc2f1f86880c33f7336b43857608ce5f99863043
Ravi theja <raviteja83@users.noreply.github.com>
2021-08-25 10:25:20 +0530

    fix: controls going below the fold (#354)

--- src/store/AppContext.js
+++ src/store/AppContext.js
@@ -56,6 +56,20 @@ const AppContextProvider = ({
   }, [hmsActions]);
 
   useEffect(() => {
+    function resetHeight() {
+      // reset the body height to that of the inner browser
+      document.body.style.height = `${window.innerHeight}px`;
+    }
+    // reset the height whenever the window's resized
+    window.addEventListener("resize", resetHeight);
+    // called to initially set the height.
+    resetHeight();
+    return () => {
+      window.removeEventListener("resize", resetHeight);
+    };
+  }, []);
+
+  useEffect(() => {
     if (!state.loginInfo.token) return;
     hmsActions.join(convertLoginInfoToJoinConfig(state.loginInfo));
     // eslint-disable-next-line
