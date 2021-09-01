09b5a749fee475673675141bb7059515f583d6f8
eswarclynn <esuclintblazer@gmail.com>
2021-09-01 23:37:18 +0530

    feat: Center, sidepane video list

--- src/views/mainGridView.jsx
+++ src/views/mainGridView.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { selectPeers, useHMSStore } from "@100mslive/hms-video-react";
 import { GridCenterView, GridSidePaneView } from "./components/gridView";
 import { AppContext } from "../store/AppContext";
-import { ROLES } from "../common/roles";
 
 export const MainGridView = ({
   isChatOpen,
@@ -10,20 +9,18 @@ export const MainGridView = ({
   role,
   isParticipantListOpen,
 }) => {
-  const { maxTileCount } = useContext(AppContext);
+  const {
+    maxTileCount,
+    appPolicyConfig: { center: centerRoles, sidepane: sidepaneRoles },
+  } = useContext(AppContext);
   const peers = useHMSStore(selectPeers);
-  const teacherPeers = peers.filter(
-    peer => peer.roleName?.toLowerCase() === ROLES.TEACHER
-  );
-  const studentPeers = peers.filter(
-    peer => peer.roleName?.toLowerCase() !== ROLES.TEACHER
+  const centerPeers = peers.filter(peer => centerRoles.includes(peer.roleName));
+  const sidebarPeers = peers.filter(peer =>
+    sidepaneRoles.includes(peer.roleName)
   );
   const hideSidePane =
-    (teacherPeers.length > 1 && studentPeers.length === 0) ||
-    (teacherPeers.length === 0 && studentPeers.length > 1);
-
-  const centerPeers = role === ROLES.TEACHER ? studentPeers : teacherPeers;
-  const sidebarPeers = role === ROLES.TEACHER ? teacherPeers : studentPeers;
+    (centerPeers.length > 1 && sidebarPeers.length === 0) ||
+    (centerPeers.length === 0 && sidebarPeers.length > 1);
 
   return (
     <React.Fragment>
