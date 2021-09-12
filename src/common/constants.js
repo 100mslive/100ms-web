34ea849267b9c8f1b258a212c671976a5a8bc526
raviteja83 <ravi.theja@100ms.live>
2021-09-12 10:59:33 +0530

    fix: add hardcoded playlist

---
+++ src/common/constants.js
@@ -0,0 +1,59 @@
+export const defaultAudioList = [
+  {
+    name: "Audio1",
+    id: "audio1",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio1.mp3",
+    type: "audio",
+  },
+  {
+    name: "Audio2",
+    id: "audio2",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio2.mp3",
+    type: "audio",
+  },
+  {
+    name: "Audio3",
+    id: "audio3",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio3.mp3",
+    type: "audio",
+  },
+  {
+    name: "Audio4",
+    id: "audio4",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio4.mp3",
+    type: "audio",
+  },
+  {
+    name: "Audio5",
+    id: "audio5",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio5.mp3",
+    type: "audio",
+  },
+  {
+    name: "Audio6",
+    id: "audio6",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/audio6.mp3",
+    type: "audio",
+  },
+];
+
+export const defaultVideoList = [
+  {
+    name: "Video1",
+    id: "video1",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/video1.mp4",
+    type: "video",
+  },
+  {
+    name: "Video2",
+    id: "video2",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/video2.mp4",
+    type: "video",
+  },
+  {
+    name: "Video3",
+    id: "video3",
+    url: "https://storage.googleapis.com/test-86284.appspot.com/video3.mp4",
+    type: "video",
+  },
+];
--- src/views/mainView.jsx
+++ src/views/mainView.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   useHMSStore,
+  useHMSActions,
+  HMSPlaylistActionType,
   selectLocalPeer,
   selectPeerSharingAudio,
   selectPeerScreenSharing,
@@ -8,6 +10,7 @@ import {
 } from "@100mslive/hms-video-react";
 import { ScreenShareView } from "./screenShareView";
 import { MainGridView } from "./mainGridView";
+import { defaultAudioList, defaultVideoList } from "../common/constants";
 
 export const ConferenceMainView = ({
   isChatOpen,
@@ -17,10 +20,21 @@ export const ConferenceMainView = ({
   const localPeer = useHMSStore(selectLocalPeer);
   const peerSharing = useHMSStore(selectPeerScreenSharing);
   const peerSharingAudio = useHMSStore(selectPeerSharingAudio);
-  const peerSharingPlaylist = useHMSStore(
-    selectPeerSharingVideoPlaylist
-  );
+  const peerSharingPlaylist = useHMSStore(selectPeerSharingVideoPlaylist);
+  const hmsActions = useHMSActions();
 
+  useEffect(() => {
+    hmsActions.performActionOnPlaylist({
+      actionType: HMSPlaylistActionType.SET_LIST,
+      list: defaultVideoList,
+      type: "video",
+    });
+    hmsActions.performActionOnPlaylist({
+      actionType: HMSPlaylistActionType.SET_LIST,
+      list: defaultAudioList,
+      type: "audio",
+    });
+  }, [hmsActions]);
 
   if (!localPeer) {
     // we don't know the role yet to decide how to render UI
