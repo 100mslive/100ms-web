ef7d889774c6fb0491b6424253b84052cd6bb7f0
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2021-09-16 15:32:29 +0530

    fix(beam): On direct join, don't render conference until connected (#390)

---
+++ src/views/components/FullPageSpinner.jsx
@@ -0,0 +1,11 @@
+import { ProgressIcon } from "@100mslive/hms-video-react";
+
+const FullPageProgress = () => (
+  <div className="h-full">
+    <div className="flex justify-center h-full items-center">
+      <ProgressIcon width="100" height="100" />
+    </div>
+  </div>
+);
+
+export default FullPageProgress;
