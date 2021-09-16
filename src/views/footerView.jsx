0ebaeef42c5984380bb66dbbe113b8a8bfe6333e
raviteja83 <ravi.theja@100ms.live>
2021-09-16 17:05:50 +0530

    fix: update usage of playlist selectors

--- src/views/footerView.jsx
+++ src/views/footerView.jsx
@@ -33,8 +33,7 @@ import {
   selectLocalPeer,
   selectScreenSharesByPeerId,
   Text,
-  HMSPlaylistType,
-  selectPlaylistCurrentSelection,
+  selectVideoPlaylist,
 } from "@100mslive/hms-video-react";
 import { useHistory, useParams } from "react-router-dom";
 import { HMSVirtualBackgroundPlugin } from "@100mslive/hms-virtual-background";
@@ -64,9 +63,7 @@ export const ConferenceFooter = ({ isChatOpen, toggleChat }) => {
   const audiopluginRef = useRef(null);
   const isAllowedToPublish = useHMSStore(selectIsAllowedToPublish);
   const permissions = useHMSStore(selectPermissions);
-  const activeVideoPlaylist = useHMSStore(
-    selectPlaylistCurrentSelection(HMSPlaylistType.video)
-  );
+  const activeVideoPlaylist = !!useHMSStore(selectVideoPlaylist.selection).id;
   const [showEndRoomModal, setShowEndRoomModal] = useState(false);
   const [shareAudioModal, setShareAudioModal] = useState(false);
   const [lockRoom, setLockRoom] = useState(false);
