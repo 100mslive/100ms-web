aeb4dd674e96ed53d0b1d9c5cc801586c7d9b928
raviteja83 <ravi.theja@100ms.live>
2021-09-20 15:57:54 +0530

    fix: show playlist for video also

--- src/views/footerView.jsx
+++ src/views/footerView.jsx
@@ -34,13 +34,13 @@ import {
   selectScreenSharesByPeerId,
   Text,
   selectVideoPlaylist,
+  VideoPlaylist,
 } from "@100mslive/hms-video-react";
 import { useHistory, useParams } from "react-router-dom";
 import { HMSVirtualBackgroundPlugin } from "@100mslive/hms-virtual-background";
 import { HMSNoiseSuppressionPlugin } from "@100mslive/hms-noise-suppression";
 import { AppContext } from "../store/AppContext";
 import { getRandomVirtualBackground } from "../common/utils";
-import { defaultVideoList } from "../common/constants";
 import { MoreSettings } from "./components/MoreSettings";
 
 export const ConferenceFooter = ({ isChatOpen, toggleChat }) => {
@@ -236,19 +236,10 @@ export const ConferenceFooter = ({ isChatOpen, toggleChat }) => {
       leftComponents.push(<AudioPlaylist key="audioPlaylist" />);
     isAllowedToPublish.screen &&
       leftComponents.push(
-        <Button
-          key="videoPlaylist"
-          iconOnly
-          variant="no-fill"
-          iconSize="md"
-          shape="rectangle"
+        <VideoPlaylist
+          trigger={<VideoPlaylistIcon />}
           active={!!activeVideoPlaylist}
-          onClick={() => {
-            hmsActions.videoPlaylist.play(defaultVideoList[0].id);
-          }}
-        >
-          <VideoPlaylistIcon />
-        </Button>
+        />
       );
   }
 
