47b0fcc8e79797116931c55b6bfccdec3476b657
raviteja83 <ravi.theja@100ms.live>
2021-10-04 16:46:41 +0530

    fix: mute all component

--- src/views/components/MuteAll.jsx
+++ src/views/components/MuteAll.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useCallback, useState } from "react";
 import {
   MessageModal,
   selectAvailableRoleNames,
@@ -8,8 +8,11 @@ import {
 } from "@100mslive/hms-video-react";
 
 const defaultClasses = {
+  form: "flex flex-col justify-center",
+  formItem: "flex items-center h-8 mb-3",
+  label: "w-1/4 mr-2",
   input:
-    "rounded-lg w-full h-full bg-gray-600 dark:bg-gray-200 focus:outline-none",
+    "rounded-lg w-1/2 h-full bg-gray-600 dark:bg-gray-200 focus:outline-none",
   checkBoxLabel: "text-sm space-x-1 flex items-center",
 };
 
@@ -17,19 +20,32 @@ export const MuteAll = () => {
   const roles = useHMSStore(selectAvailableRoleNames);
   const hmsActions = useHMSActions();
   const [showModal, setShowModal] = useState(false);
-  const [mute, setMute] = useState(false);
-  const [type, setType] = useState(false);
-  const [role, setRole] = useState(false);
-  const [source, setSource] = useState(false);
+  const [enabled, setEnabled] = useState(false);
+  const [type, setType] = useState();
+  const [role, setRole] = useState();
+  const [source, setSource] = useState();
+  const [error, setError] = useState("");
 
-  const muteAll = async () => {
+  const muteAll = useCallback(async () => {
+    if (!type && !source && !role) {
+      setError("Please select one of role, type or source");
+      return;
+    }
+    setError("");
     await hmsActions.setRemoteTracksEnabled({
-      enabled: !mute,
-      type,
+      enabled,
+      type: type,
       source,
       roles: role ? [role] : undefined,
     });
     setShowModal(false);
+  }, [role, enabled, type, source]); //eslint-disable-line
+
+  const resetState = () => {
+    setEnabled(false);
+    setType("");
+    setSource("");
+    setError("");
   };
 
   return (
@@ -39,19 +55,21 @@ export const MuteAll = () => {
           show
           onClose={() => {
             setShowModal(false);
+            resetState();
           }}
-          title="RemoteTracks State Change"
+          title="Mute/Unmute Remote Tracks"
           body={
-            <form className="flex flex-col">
-              <div className="flex items-center">
-                <div className="mr-2">Roles</div>
+            <form className={defaultClasses.form}>
+              <div className={defaultClasses.formItem}>
+                <div className={defaultClasses.label}>Roles</div>
                 <select
                   className={defaultClasses.input}
                   onChange={event => {
                     setRole(event.currentTarget.value);
+                    setError("");
                   }}
                 >
-                  <option value="">Select track type</option>
+                  <option value="">Select Role</option>
                   {roles.map(role => {
                     return (
                       <option key={role} value={role}>
@@ -61,12 +79,13 @@ export const MuteAll = () => {
                   })}
                 </select>
               </div>
-              <div className="flex items-center">
-                <div className="mr-2">Track Type</div>
+              <div className={defaultClasses.formItem}>
+                <div className={defaultClasses.label}>Track Type</div>
                 <select
                   className={defaultClasses.input}
                   onChange={event => {
                     setType(event.currentTarget.value);
+                    setError("");
                   }}
                 >
                   <option value="">Select track type</option>
@@ -74,12 +93,13 @@ export const MuteAll = () => {
                   <option value="audio">Audio</option>
                 </select>
               </div>
-              <div className="flex items-center">
-                <div className="mr-2">Track Source</div>
+              <div className={defaultClasses.formItem}>
+                <div className={defaultClasses.label}>Track Source</div>
                 <select
                   className={defaultClasses.input}
                   onChange={event => {
                     setSource(event.currentTarget.value);
+                    setError("");
                   }}
                 >
                   <option value="">Select track source</option>
@@ -92,16 +112,17 @@ export const MuteAll = () => {
               <label className={defaultClasses.checkBoxLabel}>
                 <input
                   type="checkbox"
-                  onChange={() => setMute(prev => !prev)}
-                  checked={mute}
+                  onChange={() => setEnabled(prev => !prev)}
+                  checked={enabled}
                 />
-                <span>{mute ? "Unmute" : "Mute"}</span>
+                <span>Enabled</span>
               </label>
+              <label className="text-yellow-500 mt-2">{error}</label>
             </form>
           }
           footer={
             <Button variant="emphasized" onClick={muteAll}>
-              Change Tracks State
+              Apply
             </Button>
           }
         />
@@ -109,7 +130,10 @@ export const MuteAll = () => {
       <Button
         variant="standard"
         key="muteAll"
-        onClick={muteAll}
+        onClick={() => {
+          setShowModal(true);
+          resetState();
+        }}
         classes={{ root: "mr-2" }}
       >
         Mute/Unmute All
