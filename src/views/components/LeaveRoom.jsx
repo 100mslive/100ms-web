671a8e0f08a0e023e1b52dccfb8e799d029f629f
Deepankar <deep14bhade@gmail.com>
2022-02-14 15:11:37 +0530

    refactor: replace leave popover

--- src/views/components/LeaveRoom.jsx
+++ src/views/components/LeaveRoom.jsx
@@ -1,7 +1,5 @@
 import { Fragment, useState } from "react";
 import {
-  ContextMenu,
-  ContextMenuItem,
   MessageModal,
   selectPermissions,
   useHMSActions,
@@ -9,14 +7,13 @@ import {
 } from "@100mslive/hms-video-react";
 import { useHistory, useParams } from "react-router-dom";
 import { HangUpIcon } from "@100mslive/react-icons";
-import { Button, Text } from "@100mslive/react-ui";
+import { Button, Text, Popover } from "@100mslive/react-ui";
 
 export const LeaveRoom = () => {
   const history = useHistory();
   const params = useParams();
   const [showEndRoomModal, setShowEndRoomModal] = useState(false);
   const [lockRoom, setLockRoom] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
   const permissions = useHMSStore(selectPermissions);
   const hmsActions = useHMSActions();
 
@@ -39,52 +36,19 @@ export const LeaveRoom = () => {
   };
   return (
     <Fragment>
-      <ContextMenu
-        classes={{
-          trigger: "w-auto h-auto",
-          root: "static",
-          menu: "w-56 bg-white dark:bg-gray-100",
-          menuItem: "hover:bg-transparent-0 dark:hover:bg-transparent-0",
-        }}
-        onTrigger={value => {
-          if (permissions?.endRoom) {
-            setShowMenu(value);
-          } else {
-            leaveRoom();
-          }
-        }}
-        menuOpen={showMenu}
-        key="LeaveAction"
-        trigger={
-          <Button variant="danger" key="LeaveRoom">
-            <HangUpIcon key="hangUp" />
-            <Text
-              css={{ ml: "$4", "@md": { display: "none" }, color: "$white" }}
-            >
-              Leave Room
-            </Text>
-          </Button>
-        }
-        menuProps={{
-          anchorOrigin: {
-            vertical: "top",
-            horizontal: "center",
-          },
-          transformOrigin: {
-            vertical: 128,
-            horizontal: "center",
-          },
-        }}
-      >
-        {permissions?.endRoom && (
-          <ContextMenuItem
-            label="End Room"
-            key="endRoom"
-            classes={{
-              menuTitleContainer: "hidden",
-              menuItemChildren: "my-1 w-full",
-            }}
-          >
+      {permissions.endRoom ? (
+        <Popover.Root>
+          <Popover.Trigger asChild>
+            <Button variant="danger" key="LeaveRoom">
+              <HangUpIcon key="hangUp" />
+              <Text
+                css={{ ml: "$4", "@md": { display: "none" }, color: "$white" }}
+              >
+                Leave Room
+              </Text>
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content sideOffset={10}>
             <Button
               variant="standard"
               className="w-full"
@@ -94,21 +58,22 @@ export const LeaveRoom = () => {
             >
               End Room for all
             </Button>
-          </ContextMenuItem>
-        )}
-        <ContextMenuItem
-          label="Leave Room"
-          key="leaveRoom"
-          classes={{
-            menuTitleContainer: "hidden",
-            menuItemChildren: "my-1 w-full overflow-hidden",
-          }}
-        >
-          <Button variant="danger" className="w-full" onClick={leaveRoom}>
-            Just Leave
-          </Button>
-        </ContextMenuItem>
-      </ContextMenu>
+            <Button
+              variant="danger"
+              className="w-full mt-2"
+              onClick={leaveRoom}
+            >
+              Just Leave
+            </Button>
+          </Popover.Content>
+        </Popover.Root>
+      ) : (
+        <Button variant="danger" className="w-full" onClick={leaveRoom}>
+          <HangUpIcon />
+          <Text css={{ ml: "$4", "@md": { display: "none" } }}>Leave Room</Text>
+        </Button>
+      )}
+
       <MessageModal
         show={showEndRoomModal}
         onClose={() => {
