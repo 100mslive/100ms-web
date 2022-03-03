e6c38f8cad5c242e1a6ca6b95d2a28db34988edc
raviteja83 <ravi.theja@100ms.live>
2022-03-03 20:42:30 +0530

    feat(app): add controls to videoplayer

--- src/views/new/Playlist/VideoPlayer.jsx
+++ src/views/new/Playlist/VideoPlayer.jsx
@@ -1,11 +1,36 @@
-import React from "react";
-import { Box, Video } from "@100mslive/react-ui";
+import React, { useRef } from "react";
+import { useFullscreen, useToggle } from "react-use";
 import {
   selectVideoPlaylistVideoTrackByPeerID,
   useHMSStore,
 } from "@100mslive/react-sdk";
+import { ShrinkIcon, ExpandIcon } from "@100mslive/react-icons";
+import { Box, IconButton, Video } from "@100mslive/react-ui";
+import { VideoPlaylistControls } from "./PlaylistControls";
 
 export const VideoPlayer = ({ peerId }) => {
   const videoTrack = useHMSStore(selectVideoPlaylistVideoTrackByPeerID(peerId));
-  return <Video trackId={videoTrack?.id} />;
+  const ref = useRef(null);
+  const [show, toggle] = useToggle(false);
+  const isFullscreen = useFullscreen(ref, show, {
+    onClose: () => toggle(false),
+  });
+  return (
+    <Box css={{ position: "relative", w: "100%", h: "100%" }} ref={ref}>
+      <Video trackId={videoTrack?.id} />
+      <VideoPlaylistControls>
+        <IconButton
+          onClick={() => toggle()}
+          css={{
+            color: "$white",
+            height: "max-content",
+            alignSelf: "center",
+            cursor: "pointer",
+          }}
+        >
+          {isFullscreen ? <ShrinkIcon /> : <ExpandIcon />}
+        </IconButton>
+      </VideoPlaylistControls>
+    </Box>
+  );
 };
