c0693f7ec21c2655abc3be03e23ebd8e662cdf6f
raviteja83 <ravi.theja@100ms.live>
2022-03-03 20:41:18 +0530

    refactor(app): playlist control,add video control

--- src/views/new/Playlist/PlaylistControls.jsx
+++ src/views/new/Playlist/PlaylistControls.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import {
   HMSPlaylistType,
   selectAudioPlaylist,
+  selectAudioTrackVolume,
+  selectPeerSharingVideoPlaylist,
   selectVideoPlaylist,
+  selectVideoPlaylistAudioTrackByPeerID,
   useHMSActions,
   useHMSStore,
 } from "@100mslive/react-sdk";
@@ -12,10 +15,12 @@ import {
   PlayIcon,
   PauseIcon,
   PlaylistIcon,
+  SpeakerIcon,
 } from "@100mslive/react-icons";
 import { Box, Flex, IconButton, Slider, Text } from "@100mslive/react-ui";
+import { usePlaylist } from "../../hooks/usePlaylist";
 
-const PlaylistProgress = ({ type, duration }) => {
+const Progress = ({ type, duration }) => {
   const selectPlaylist =
     type === HMSPlaylistType.audio ? selectAudioPlaylist : selectVideoPlaylist;
   const progress = useHMSStore(selectPlaylist.progress);
@@ -40,66 +45,144 @@ const PlaylistProgress = ({ type, duration }) => {
   );
 };
 
-export const PlaylistControls = ({ type, onToggle }) => {
-  const selector =
-    type === HMSPlaylistType.audio ? selectAudioPlaylist : selectVideoPlaylist;
+export const PlaylistActive = ({ type, onToggle }) => {
+  const isAudioPlaylist = type === HMSPlaylistType.audio;
+  const selector = isAudioPlaylist ? selectAudioPlaylist : selectVideoPlaylist;
   const active = useHMSStore(selector.selectedItem);
-  const selection = useHMSStore(selector.selection);
-  const hmsActions = useHMSActions();
-  const playlistAction =
-    type === HMSPlaylistType.audio
-      ? hmsActions.audioPlaylist
-      : hmsActions.videoPlaylist;
   if (!active) {
     return null;
   }
   return (
-    <Box css={{ p: "$8", borderTop: "1px solid $borderLight", bg: "$menuBg" }}>
-      <Flex justify="center">
-        <IconButton
-          disabled={!selection.hasPrevious}
-          onClick={() => {
-            playlistAction.playPrevious();
-          }}
-        >
-          <PrevIcon />
-        </IconButton>
-        <IconButton
-          onClick={() => {
-            active.playing
-              ? playlistAction.pause()
-              : playlistAction.play(active.id);
-          }}
-        >
-          {active.playing ? (
-            <PauseIcon width={32} height={32} />
-          ) : (
-            <PlayIcon width={32} height={32} />
+    <Box css={{ mt: "$8" }}>
+      <Flex justify="between" css={{ w: "100%" }}>
+        <Box>
+          <Text variant="md">{active.name}</Text>
+          {active.metadata?.description && (
+            <Text variant="xs">{active.metadata?.description}</Text>
           )}
-        </IconButton>
-        <IconButton
-          disabled={!selection.hasNext}
-          onClick={() => {
-            playlistAction.playNext();
-          }}
-        >
-          <NextIcon />
+        </Box>
+        <IconButton onClick={onToggle} css={{ alignSelf: "center" }}>
+          <PlaylistIcon />
         </IconButton>
       </Flex>
-      <PlaylistProgress type={type} duration={active.duration} />
-      <Box css={{ mt: "$8" }}>
-        <Flex justify="between" css={{ w: "100%" }}>
-          <Box>
-            <Text variant="md">{active.name}</Text>
-            {active.metadata?.description && (
-              <Text variant="xs">{active.metadata?.description}</Text>
-            )}
-          </Box>
-          <IconButton onClick={onToggle} css={{ alignSelf: "center" }}>
-            <PlaylistIcon />
-          </IconButton>
-        </Flex>
-      </Box>
+    </Box>
+  );
+};
+
+const Controls = ({ type, css = {} }) => {
+  const { active, hasNext, hasPrevious, actions } = usePlaylist(type);
+  if (!active) {
+    return null;
+  }
+  return (
+    <Flex justify="center" css={css}>
+      <IconButton
+        disabled={!hasPrevious}
+        onClick={() => {
+          actions.playPrevious();
+        }}
+      >
+        <PrevIcon />
+      </IconButton>
+      <IconButton
+        onClick={() => {
+          active.playing ? actions.pause() : actions.play(active.id);
+        }}
+      >
+        {active.playing ? (
+          <PauseIcon width={32} height={32} />
+        ) : (
+          <PlayIcon width={32} height={32} />
+        )}
+      </IconButton>
+      <IconButton
+        disabled={!hasNext}
+        onClick={() => {
+          actions.playNext();
+        }}
+      >
+        <NextIcon />
+      </IconButton>
+    </Flex>
+  );
+};
+
+const VolumeControl = () => {
+  const hmsActions = useHMSActions();
+  const volume = useHMSStore(selectVideoPlaylist.volume);
+  const active = useHMSStore(selectVideoPlaylist.selectedItem);
+  const peerSharingPlaylist = useHMSStore(selectPeerSharingVideoPlaylist);
+  const audioTrack = useHMSStore(
+    selectVideoPlaylistAudioTrackByPeerID(peerSharingPlaylist?.id)
+  );
+  const audioTrackVolume = useHMSStore(selectAudioTrackVolume(audioTrack?.id));
+  const sliderVolume = active ? volume : audioTrackVolume;
+
+  return (
+    <Flex align="center" css={{ color: "$white" }}>
+      <SpeakerIcon width={24} height={24} />
+      <Slider
+        css={{ mx: "$4", w: "$20" }}
+        min={0}
+        max={100}
+        step={1}
+        value={[Math.floor(sliderVolume)]}
+        onValueChange={e => {
+          const value = e[0];
+          if (active) {
+            hmsActions.videoPlaylist.setVolume(value);
+          } else if (audioTrack) {
+            hmsActions.setVolume(value, audioTrack.id);
+          }
+        }}
+      />
+    </Flex>
+  );
+};
+
+export const AudioPlaylistControls = ({ onToggle }) => {
+  const { active } = usePlaylist(HMSPlaylistType.audio);
+  if (!active) {
+    return null;
+  }
+  return (
+    <Box
+      css={{
+        p: "$8",
+        borderTop: "1px solid $borderLight",
+        bg: "$menuBg",
+      }}
+    >
+      <Controls type={HMSPlaylistType.audio} />
+      <Progress type={HMSPlaylistType.audio} duration={active.duration} />
+      <PlaylistActive type={HMSPlaylistType.audio} onToggle={onToggle} />
+    </Box>
+  );
+};
+
+export const VideoPlaylistControls = ({ children }) => {
+  const { active } = usePlaylist(HMSPlaylistType.video);
+
+  if (!active) {
+    return null;
+  }
+
+  return (
+    <Box
+      css={{
+        p: "$8",
+        position: "absolute",
+        bottom: 0,
+        boxShadow: "$sm",
+        w: "100%",
+      }}
+    >
+      <Progress type={HMSPlaylistType.video} duration={active.duration} />
+      <Flex align="center">
+        <VolumeControl />
+        <Controls css={{ flex: "1 1 0" }} />
+        {children}
+      </Flex>
     </Box>
   );
 };
