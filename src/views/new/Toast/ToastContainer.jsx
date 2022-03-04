408f82e6659032cb7e0cdb32829c1a5ae6fd53d2
raviteja83 <ravi.theja@100ms.live>
2022-03-04 12:47:21 +0530

    refactor(app): remove unnecessary usetoast hook

--- src/views/new/Toast/ToastContainer.jsx
+++ src/views/new/Toast/ToastContainer.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { selectIsConnectedToRoom, useHMSStore } from "@100mslive/react-sdk";
 import { Toast as ToastPrimitive } from "@100mslive/react-ui";
 import { Toast } from "./Toast";
-import { useToast } from "./useToast";
 import { MAX_TOASTS } from "../../../common/constants";
 import { ToastManager } from "./ToastManager";
 
 export const ToastContainer = () => {
-  const { toasts } = useToast();
   const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const [toasts, setToast] = useState([]);
+  useEffect(() => {
+    ToastManager.addListener(setToast);
+    return () => {
+      ToastManager.removeListener(setToast);
+    };
+  }, []);
   return (
     <ToastPrimitive.Provider swipeDirection="left" duration={3000}>
       {toasts.slice(0, MAX_TOASTS).map(toast => {
