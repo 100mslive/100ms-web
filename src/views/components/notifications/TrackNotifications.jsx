e934e8f53fefb8134e0c47c04565fb8561388d25
raviteja83 <ravi.theja@100ms.live>
2022-03-04 13:06:13 +0530

    fix(app): optimise track notifciations

--- src/views/components/notifications/TrackNotifications.jsx
+++ src/views/components/notifications/TrackNotifications.jsx
@@ -4,13 +4,14 @@ import {
   useHMSNotifications,
 } from "@100mslive/react-sdk";
 
+const notificationTypes = [
+  HMSNotificationTypes.TRACK_ADDED,
+  HMSNotificationTypes.TRACK_REMOVED,
+  HMSNotificationTypes.TRACK_MUTED,
+  HMSNotificationTypes.TRACK_UNMUTED,
+];
 export const TrackNotifications = () => {
-  const notification = useHMSNotifications([
-    HMSNotificationTypes.TRACK_ADDED,
-    HMSNotificationTypes.TRACK_REMOVED,
-    HMSNotificationTypes.TRACK_MUTED,
-    HMSNotificationTypes.TRACK_UNMUTED,
-  ]);
+  const notification = useHMSNotifications(notificationTypes);
   useEffect(() => {
     if (notification) {
       console.debug(`[${notification.type}]`, notification);
