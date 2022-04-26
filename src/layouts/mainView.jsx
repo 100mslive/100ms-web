75d9f7e0aa21309ed015b973c370fb843499ffb8
Ravi theja <raviteja83@users.noreply.github.com>
2022-04-26 12:10:57 +0530

    fix(app): don't lazy load screenshare/leave

--- src/layouts/mainView.jsx
+++ src/layouts/mainView.jsx
@@ -12,6 +12,7 @@ import {
 import { MainGridView } from "./mainGridView";
 import { AppContext } from "../components/context/AppContext";
 import FullPageProgress from "../components/FullPageProgress";
+import ScreenShareView from "./screenShareView";
 import { useWhiteboardMetadata } from "../plugins/whiteboard";
 import { useBeamAutoLeave } from "../common/hooks";
 import { UI_MODE_ACTIVE_SPEAKER, UI_SETTINGS } from "../common/constants";
@@ -19,7 +20,6 @@ import { useUISettings } from "../components/AppData/useUISettings";
 
 const WhiteboardView = React.lazy(() => import("./WhiteboardView"));
 const HLSView = React.lazy(() => import("./HLSView"));
-const ScreenShareView = React.lazy(() => import("./screenShareView"));
 const ActiveSpeakerView = React.lazy(() => import("./ActiveSpeakerView"));
 
 export const ConferenceMainView = ({ isChatOpen, toggleChat }) => {
