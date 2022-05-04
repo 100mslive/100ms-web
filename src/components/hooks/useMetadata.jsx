517b04e125c5a3d6fc5a58335b4ca747f7f11cbc
raviteja83 <ravi.theja@100ms.live>
2022-05-04 14:07:45 +0530

    fix(app): useMetadata hook

--- src/components/hooks/useMetadata.jsx
+++ src/components/hooks/useMetadata.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import {
   useHMSActions,
   useHMSStore,
@@ -9,28 +10,43 @@ export const useMyMetadata = () => {
   const hmsActions = useHMSActions();
   const localPeerId = useHMSStore(selectLocalPeerID);
   const metaData = useHMSStore(selectPeerMetadata(localPeerId));
-  let isHandRaised = metaData?.isHandRaised || false;
-  let isBRBOn = metaData?.isBRBOn || false; // BRB = be right back
+  const [isHandRaised, setHandRaised] = useState(
+    metaData?.isHandRaised || false
+  );
+  const [isBRBOn, setBRBOn] = useState(metaData?.isBRBOn || false); // BRB = be right back
 
   const update = async updatedFields => {
     try {
       await hmsActions.changeMetadata(Object.assign(metaData, updatedFields));
+      return true;
     } catch (error) {
       console.error("failed to update metadata ", metaData, updatedFields);
     }
   };
 
-  const toggleHandRaise = async () => {
-    isHandRaised = !isHandRaised;
-    isBRBOn = isHandRaised ? false : isBRBOn; // turn off brb if hand is raised
-    await update({ isHandRaised, isBRBOn });
-  };
+  const toggleHandRaise = useCallback(async () => {
+    const brbUpdate = !isHandRaised ? false : isBRBOn;
+    const success = await update({
+      isHandRaised: !isHandRaised,
+      isBRBOn: brbUpdate,
+    });
+    if (success) {
+      setBRBOn(brbUpdate);
+      setHandRaised(!isHandRaised);
+    }
+  }, [isHandRaised, isBRBOn]); //eslint-disable-line
 
-  const toggleBRB = async () => {
-    isBRBOn = !isBRBOn;
-    isHandRaised = isBRBOn ? false : isHandRaised; // turn off hand raise if user is going away(brb on)
-    await update({ isHandRaised, isBRBOn });
-  };
+  const toggleBRB = useCallback(async () => {
+    const handRaiseUpdate = !isBRBOn ? false : isHandRaised;
+    const success = await update({
+      isHandRaised: handRaiseUpdate,
+      isBRBOn: !isBRBOn,
+    });
+    if (success) {
+      setBRBOn(!isBRBOn);
+      setHandRaised(handRaiseUpdate);
+    }
+  }, [isHandRaised, isBRBOn]); //eslint-disable-line
 
   return {
     isHandRaised,
