52a02c4ac73990fb5a6f65a39911377270b00122
Tushar <tushar@100ms.live>
2022-05-09 14:25:56 +0530

    fix: video tile not showing for blank peer name

--- src/components/VideoTile.jsx
+++ src/components/VideoTile.jsx
@@ -52,7 +52,7 @@ const Tile = ({ peerId, showStatsOnTiles, width, height, index }) => {
       css={{ width, height }}
       data-testid={`participant_tile_${index}`}
     >
-      {peerName ? (
+      {peerName !== undefined ? (
         <StyledVideoTile.Container
           onMouseEnter={onHoverHandler}
           onMouseLeave={onHoverHandler}
