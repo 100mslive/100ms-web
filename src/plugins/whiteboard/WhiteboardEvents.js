6623fc1a8712ca68c127db1f1eeaf6d2726667a5
eswarclynn <esuclintblazer@gmail.com>
2022-05-16 19:46:04 +0530

    fix(app): remove old whiteboard files

--- src/plugins/whiteboard/WhiteboardEvents.js
+++
@@ -1,8 +0,0 @@
-export const WhiteboardEvents = {
-  // To broadcast new changes made in whiteboard
-  STATE_CHANGE: "state-change",
-  // To broadcast the current whole state of the board by the owner
-  CURRENT_STATE: "current-state",
-  // For newly joined peers to request current state from owner
-  REQUEST_STATE: "request-state",
-};
