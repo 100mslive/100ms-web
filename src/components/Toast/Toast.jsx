1c6792e631577280e1f5727513fd7008a9ae7577
hardik1003 <93115614+hardik1003@users.noreply.github.com>
2022-07-12 15:50:56 +0530

    Style: Toast UI update

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -8,22 +8,19 @@ export const Toast = ({
   open,
   duration,
   onOpenChange,
+  icon,
+  ...props
 }) => {
   return (
-    <ToastPrimitive.Root
+    <ToastPrimitive.HMSToast
+      title={title}
+      description={description}
       open={open}
+      isClosable={close}
       onOpenChange={onOpenChange}
       duration={!close ? 600000 : duration}
-    >
-      <ToastPrimitive.Title css={{ mr: close ? "$12" : 0 }}>
-        {title}
-      </ToastPrimitive.Title>
-      {description && (
-        <ToastPrimitive.Description css={{ mr: close ? "$12" : 0 }}>
-          {description}
-        </ToastPrimitive.Description>
-      )}
-      {close && <ToastPrimitive.Close />}
-    </ToastPrimitive.Root>
+      icon={icon}
+      {...props}
+    />
   );
 };
--- src/components/Toast/ToastConfig.js
+++ src/components/Toast/ToastConfig.js
@@ -1,102 +1,90 @@
 import { HandIcon, PersonIcon, ChatIcon } from "@100mslive/react-icons";
-import { TextWithIcon } from "../Notifications/TextWithIcon";
 
 export const ToastConfig = {
   PEER_LIST: {
     single: function (notification) {
       if (notification.data.length === 1) {
-        return (
-          <TextWithIcon
-            Icon={PersonIcon}
-          >{`${notification.data[0]?.name} joined`}</TextWithIcon>
-        );
+        return {
+          title: `${notification.data[0]?.name} joined`,
+          icon: <PersonIcon />,
+        };
       }
-      return (
-        <TextWithIcon Icon={PersonIcon}>
-          {`${notification.data[notification.data.length - 1]?.name} and ${
-            notification.data.length - 1
-          } others joined`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notification.data[notification.data.length - 1]?.name} and ${
+          notification.data.length - 1
+        } others joined`,
+        icon: <PersonIcon />,
+      };
     },
     multiple: notifications => {
-      return (
-        <TextWithIcon Icon={PersonIcon}>
-          {`${notifications[0].data.name} and ${
-            notifications.length - 1
-          } others joined`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notifications[0].data.name} and ${
+          notifications.length - 1
+        } others joined`,
+        icon: <PersonIcon />,
+      };
     },
   },
   PEER_JOINED: {
     single: function (notification) {
-      return (
-        <TextWithIcon
-          Icon={PersonIcon}
-        >{`${notification.data?.name} joined`}</TextWithIcon>
-      );
+      return {
+        title: `${notification.data?.name} joined`,
+        icon: <PersonIcon />,
+      };
     },
     multiple: function (notifications) {
-      return (
-        <TextWithIcon Icon={PersonIcon}>
-          {`${notifications[notifications.length - 1].data.name} and ${
-            notifications.length - 1
-          } others joined`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notifications[notifications.length - 1].data.name} and ${
+          notifications.length - 1
+        } others joined`,
+        icon: <PersonIcon />,
+      };
     },
   },
   PEER_LEFT: {
     single: function (notification) {
-      return (
-        <TextWithIcon
-          Icon={PersonIcon}
-        >{`${notification.data?.name} left`}</TextWithIcon>
-      );
+      return {
+        title: `${notification.data?.name} left`,
+        icon: <PersonIcon />,
+      };
     },
     multiple: function (notifications) {
-      return (
-        <TextWithIcon Icon={PersonIcon}>
-          {`${notifications[notifications.length - 1].data.name} and ${
-            notifications.length - 1
-          } others left`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notifications[notifications.length - 1].data.name} and ${
+          notifications.length - 1
+        } others left`,
+        icon: <PersonIcon />,
+      };
     },
   },
   METADATA_UPDATED: {
     single: notification => {
-      return (
-        <TextWithIcon
-          Icon={HandIcon}
-        >{`${notification.data?.name} raised hand`}</TextWithIcon>
-      );
+      return {
+        title: `${notification.data?.name} raised hand`,
+        icon: <HandIcon />,
+      };
     },
     multiple: notifications => {
-      return (
-        <TextWithIcon Icon={HandIcon}>
-          {`${notifications[notifications.length - 1].data?.name} and ${
-            notifications.length - 1
-          } others raised hand`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notifications[notifications.length - 1].data?.name} and ${
+          notifications.length - 1
+        } others raised hand`,
+        icon: <HandIcon />,
+      };
     },
   },
   NEW_MESSAGE: {
     single: notification => {
-      return (
-        <TextWithIcon
-          Icon={ChatIcon}
-        >{`New message from ${notification.data?.senderName}`}</TextWithIcon>
-      );
+      return {
+        title: `New message from ${notification.data?.senderName}`,
+        icon: <ChatIcon />,
+      };
     },
     multiple: notifications => {
-      return (
-        <TextWithIcon Icon={ChatIcon}>
-          {`${notifications.length} new messages`}
-        </TextWithIcon>
-      );
+      return {
+        title: `${notifications.length} new messages`,
+        icon: <ChatIcon />,
+      };
     },
   },
 };
