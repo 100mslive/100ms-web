cb52c2a018632ab94e44585d87120d4d235471e5
raviteja83 <ravi.theja@100ms.live>
2022-07-12 16:06:56 +0530

    fix: reduce size of change role

--- src/components/Header/ParticipantList.jsx
+++ src/components/Header/ParticipantList.jsx
@@ -258,7 +258,7 @@ const ParticipantMoreActions = ({ onRoleChange, peerId }) => {
           <VerticalMenuIcon />
         </Text>
       </Dropdown.Trigger>
-      <Dropdown.Content align="start" sideOffset={8}>
+      <Dropdown.Content align="start" sideOffset={8} css={{ w: "$64" }}>
         {canChangeRole && (
           <Dropdown.Item onClick={() => onRoleChange(peerId)}>
             <ChangeRoleIcon />
