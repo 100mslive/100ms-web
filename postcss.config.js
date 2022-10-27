3772ee6ed4ec1dd11671f024511ca2dc6a3b7b2d
raviteja83 <ravi.theja@100ms.live>
2022-10-27 16:22:29 +0530

    feat: tailwind setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
     cssnano: {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx, js}", "./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
