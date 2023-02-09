c2782f8634dd3ab78f77cd2a53d00cf7b3084e37
harshil-100ms <123355266+harshil-100ms@users.noreply.github.com>
2023-02-09 14:08:52 +0530

    fix: set recording properties false instead of undefined

--- src/components/Streaming/HLSStreaming.jsx
+++ src/components/Streaming/HLSStreaming.jsx
@@ -48,9 +48,7 @@ const StartHLS = () => {
         setError("");
         await hmsActions.startHLSStreaming({
           variants,
-          recording: record
-            ? { hlsVod: true, singleFilePerLayer: true }
-            : undefined,
+          recording: { hlsVod: record, singleFilePerLayer: record },
         });
       } catch (error) {
         if (error.message.includes("invalid input")) {
