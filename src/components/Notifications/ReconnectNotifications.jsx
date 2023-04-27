f8785733816ad5456d2a48066e8966b30de575a0
harshil-100ms <123355266+harshil-100ms@users.noreply.github.com>
2023-04-27 16:23:46 +0530

    fix: revert reconnecting spinner changes

--- src/components/Notifications/ReconnectNotifications.jsx
+++ src/components/Notifications/ReconnectNotifications.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { logMessage } from "zipyai";
 import {
   HMSNotificationTypes,
   useHMSNotifications,
 } from "@100mslive/react-sdk";
-import { Dialog, Flex, Loading, Text } from "@100mslive/react-ui";
 import { ToastConfig } from "../Toast/ToastConfig";
 import { ToastManager } from "../Toast/ToastManager";
 
@@ -15,7 +14,6 @@ const notificationTypes = [
 let notificationId = null;
 export const ReconnectNotifications = () => {
   const notification = useHMSNotifications(notificationTypes);
-  const [open, setOpen] = useState(false);
   useEffect(() => {
     if (notification?.type === HMSNotificationTypes.RECONNECTED) {
       logMessage("Reconnected");
@@ -23,40 +21,13 @@ export const ReconnectNotifications = () => {
         notificationId,
         ToastConfig.RECONNECTED.single()
       );
-      setOpen(false);
     } else if (notification?.type === HMSNotificationTypes.RECONNECTING) {
       logMessage("Reconnecting");
-      ToastManager.removeToast(notificationId);
-      setOpen(true);
+      notificationId = ToastManager.replaceToast(
+        notificationId,
+        ToastConfig.RECONNECTING.single(notification.data.message)
+      );
     }
   }, [notification]);
-  if (!open) return null;
-  return (
-    <Dialog.Root open={open} modal={true}>
-      <Dialog.Portal container={document.getElementById("conferencing")}>
-        <Dialog.Overlay />
-        <Dialog.Content
-          css={{
-            width: "fit-content",
-            maxWidth: "80%",
-            p: "$4 $8",
-            position: "relative",
-            top: "unset",
-            bottom: "$9",
-            transform: "translate(-50%, -100%)",
-            animation: "none !important",
-          }}
-        >
-          <Flex align="center">
-            <div style={{ display: "inline", margin: "0.25rem" }}>
-              <Loading size={16} />
-            </div>
-            <Text css={{ fontSize: "$space$8", color: "$textHighEmp" }}>
-              You lost your network connection. Trying to reconnect.
-            </Text>
-          </Flex>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
+  return null;
 };
