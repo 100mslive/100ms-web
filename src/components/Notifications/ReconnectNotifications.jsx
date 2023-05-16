87258590ef91f36c232a6524e859c532806eedcc
Ravi theja <raviteja83@users.noreply.github.com>
2023-05-16 14:32:00 +0530

    feat: show a reconnecting spinner

--- src/components/Notifications/ReconnectNotifications.jsx
+++ src/components/Notifications/ReconnectNotifications.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { logMessage } from "zipyai";
 import {
   HMSNotificationTypes,
   useHMSNotifications,
 } from "@100mslive/react-sdk";
+import { Dialog, Flex, Loading, Text } from "@100mslive/react-ui";
 import { ToastConfig } from "../Toast/ToastConfig";
 import { ToastManager } from "../Toast/ToastManager";
 
@@ -14,6 +15,7 @@ const notificationTypes = [
 let notificationId = null;
 export const ReconnectNotifications = () => {
   const notification = useHMSNotifications(notificationTypes);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     if (notification?.type === HMSNotificationTypes.RECONNECTED) {
       logMessage("Reconnected");
@@ -21,13 +23,40 @@ export const ReconnectNotifications = () => {
         notificationId,
         ToastConfig.RECONNECTED.single()
       );
+      setOpen(false);
     } else if (notification?.type === HMSNotificationTypes.RECONNECTING) {
       logMessage("Reconnecting");
-      notificationId = ToastManager.replaceToast(
-        notificationId,
-        ToastConfig.RECONNECTING.single(notification.data.message)
-      );
+      ToastManager.removeToast(notificationId);
+      setOpen(true);
     }
   }, [notification]);
-  return null;
+  if (!open) return null;
+  return (
+    <Dialog.Root open={open} modal={true}>
+      <Dialog.Portal container={document.getElementById("conferencing")}>
+        <Dialog.Overlay />
+        <Dialog.Content
+          css={{
+            width: "fit-content",
+            maxWidth: "80%",
+            p: "$4 $8",
+            position: "relative",
+            top: "unset",
+            bottom: "$9",
+            transform: "translate(-50%, -100%)",
+            animation: "none !important",
+          }}
+        >
+          <Flex align="center">
+            <div style={{ display: "inline", margin: "0.25rem" }}>
+              <Loading size={16} />
+            </div>
+            <Text css={{ fontSize: "$space$8", color: "$textHighEmp" }}>
+              You lost your network connection. Trying to reconnect.
+            </Text>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 };
