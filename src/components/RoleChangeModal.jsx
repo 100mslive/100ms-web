96e37b2da0303a94276767bd5a902aef289083c3
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2023-07-24 20:59:47 +0530

    fix: padding in role change cancel ui (#1522)

--- src/components/RoleChangeModal.jsx
+++ src/components/RoleChangeModal.jsx
@@ -184,7 +184,7 @@ export const RoleChangeModal = ({ peerId, onOpenChange }) => {
             css={{ width: "100%", gap: "$md" }}
           >
             <Box css={{ width: "50%" }}>
-              <Dialog.Close css={{ width: "100%" }} asChild>
+              <Dialog.Close css={{ width: "100%", p: "$4 $8" }} asChild>
                 <Button
                   variant="standard"
                   outlined
