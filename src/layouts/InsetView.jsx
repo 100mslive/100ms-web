480aff6ecb4e6931cd395cd7574720f892e8bbb3
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-08-03 10:21:02 +0530

    fixes: WEB-1851, WEB-1855, WEB-1856 (#1548)
    * fix: content parity
    
    * fix: toast bg
    
    * fix: lint
    
    * fix: enum usage

--- src/layouts/InsetView.jsx
+++ src/layouts/InsetView.jsx
@@ -225,9 +225,6 @@ const InsetTile = ({ isMobile, roleMap, isLandscape }) => {
             size: "100%",
             padding: 0,
           }}
-          containerCSS={{
-            bg: "$surface_default",
-          }}
           width={width}
           height={height}
         />
