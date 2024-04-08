ac93e236bc9486ff890cdad376cb31a8bd8df3b7
Spandan Joshi <spandanj685@gmail.com>
2024-04-08 20:46:16 +0530

    demo attendence cron issue

--- src/components/PreviewScreen.jsx
+++ src/components/PreviewScreen.jsx
@@ -56,8 +56,7 @@ const PreviewScreen = React.memo(({ authTokenByRoomCodeEndpoint }) => {
   //skip preview if peer to peer session is true
   const storedLearnerPeerValue = localStorage.getItem('isPeerLearner');
   const storedInstantDemoValue = localStorage.getItem('isInstantDemo');
-  console.log({storedInstantDemoValue})
-  skipPreview = (storedLearnerPeerValue || storedInstantDemoValue) === 'true';
+  skipPreview = (storedLearnerPeerValue  === 'true') || (storedInstantDemoValue === 'true');
 
   console.log("Skip preview", skipPreview , directJoinHeadful)
 
